b928810d11a7a6a6bd1bb2cee8f9aaff45e11f39
Seal <a.i.karpov98@gmail.com>
2018-10-14 23:41:52 +0300

    Repository, Singleton realisation

--- main/java/by/training/epam/entity/factory/FiguresFabric.java
+++
@@ -1,9 +0,0 @@
-package by.training.epam.entity.factory;
-
-import by.training.epam.entity.RoundFigure;
-
-public abstract class FiguresFabric {
-	
-	public abstract RoundFigure createFigure();
-	
-}
--- main/java/by/training/epam/observer/RoundFigureObserver.java
+++
@@ -1,7 +0,0 @@
-package by.training.epam.observer;
-
-public interface RoundFigureObserver {
-	
-	void updateFigure(int ballId, double ballVolume, double ballSurfa);
-	
-}
